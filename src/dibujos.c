7f5410710e6dad83464b50d86c66e4016b262fbd
PabloUnican <160730562+PabloUnican@users.noreply.github.com>
2024-12-10 15:52:15 +0100

    modificado funcionamiento crear y mover tuberias

--- src/dibujos.c
+++ src/dibujos.c
@@ -11,12 +11,12 @@ typedef struct dibujo{
     struct color * dibujo;
     int x, y;
     struct position posi;
-}
+};
 
 typedef struct tuberia{
     struct dibujo * tube;
-    struct dibujo * hueco;
-}
+    int hueco_arriba, hueco_abajo;
+};
 
 #define A {0x00, 0x00, 0xf0}
 #define B {0x00, 0x00, 0x80}
@@ -154,11 +154,14 @@ struct color image_tuberia[120][26] =
     {N,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,N},
     {N,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,N},
     {N,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,N},
-    {N,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,N},
     {N,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,V,N}
 };
 
-struct tuberia creaTuberia(int x, int hueco_abajo, int hueco_arriba)
+#define MAX_TUBERIAS 10
+struct tuberia tuberias[MAX_TUBERIAS];
+int indice_tuberia = 0;
+
+struct tuberia creaTuberia(int hueco_abajo, int hueco_arriba)
 {
     //crear tuberia
     struct tuberia tuberia;
@@ -167,30 +170,37 @@ struct tuberia creaTuberia(int x, int hueco_abajo, int hueco_arriba)
     tube.dibujo = image_tuberia;
     tube.x = 26;
     tube.y = 120;
-    tube.posi.x = x;
+    tube.posi.x = 160;
     tube.posi.y = 0;
-    tuberia.tube = &tube;
-    //crear hueco
-    struct dibujo hueco;
-    hueco.x = 26;
-    hueco.y = hueco_abajo - hueco_arriba;
-    struct color image_hueco[hueco.y][26];
-    //rellenar hueco
-    for (int i = 0; i < hueco.y; i++)
+
+    //modificar agujero en tubo
+    for (int i = hueco_arriba; i < hueco_abajo; i++)
     {
-        for (int j = 0; j < hueco.x; j++)
+        for (int j = 0; j < 26; j++)
         {
-            image_hueco[i][j] = M;
+            tube.dibujo[i][j] = M;
         }
     }
-    hueco.posi.x = x;
-    hueco.posi.y = hueco_arriba;
-    tuberia.hueco = &hueco;
-    return tuberia; 
+
+    tuberia.tube = &tube;
+
+    //crear hueco
+    tuberia.hueco_abajo = hueco_abajo;
+    tuberia.hueco_arriba = hueco_arriba;
+    
+    //guarda tuberia
+    tuberias[indice_tuberia % MAX_TUBERIAS] = tuberia;
+    indice_tuberia++;
+
+    return tuberia;
 }
 
-void mueveTuberia(struct dibujo tuberia, int distancia)
+void mueveTuberias(struct dibujo tuberia, int distancia)
 {
-
-
+    for (int i = 0; i < MAX_TUBERIAS; i++)
+    {
+        tuberias[i].tube->posi.x -= distancia;
+        
+    }
+    //llama a pintar
 }
